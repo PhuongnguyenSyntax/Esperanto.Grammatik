3679926719997a4a22e485d23ffa8a4c069d7d8e
FlamaHenkel <phuongnguyen.ibk@gmail.com>
2024-03-16 14:01:58 +0100

    Flama commit inhalt

---
+++ Esperanto.Grammatik/Aussprache/testUI2.swift
@@ -0,0 +1,18 @@
+//
+//  testUI2.swift
+//  Esperanto.Grammatik
+//
+//  Created by Flama Henkel on 14.03.24.
+//
+
+import SwiftUI
+
+struct testUI2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    testUI2()
+}
---
+++ Esperanto.Grammatik/Aussprache/testUI3.swift
@@ -0,0 +1,18 @@
+//
+//  testUI3.swift
+//  Esperanto.Grammatik
+//
+//  Created by Flama Henkel on 15.03.24.
+//
+
+import SwiftUI
+
+struct testUI3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    testUI3()
+}

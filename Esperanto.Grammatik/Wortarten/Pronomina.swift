704882afacff63cc5db36681754364922b36863d
FlamaHenkel <phuongnguyen.ibk@gmail.com>
2024-03-18 09:36:52 +0100

    commit

--- Esperanto.Grammatik/Wortarten/Pronomina.swift
+++ Esperanto.Grammatik/Wortarten/Pronomina.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct Pronomina: View {
+    
+    struct Pronomen: Identifiable {
+        let column1: String
+        let column2: String
+        let column3: String
+        let column4: String
+        let column5: String
+        let column6: String
+        let id = UUID()
+    }
+    
+  /*  @State private var pronomen = [
+    Pronomen(column1: <#T##String#>, column2: <#T##String#>, column3: <#T##String#>, column4: <#T##String#>, column5: <#T##String#>, column6: <#T##String#>),
+    Pronomen(column1: "", column2: <#T##String#>, column3: <#T##String#>, column4: <#T##String#>, column5: <#T##String#>, column6: <#T##String#>),
+    Pronomen(column1: <#T##String#>, column2: <#T##String#>, column3: <#T##String#>, column4: <#T##String#>, column5: <#T##String#>, column6: <#T##String#>),
+    Pronomen(column1: <#T##String#>, column2: <#T##String#>, column3: <#T##String#>, column4: <#T##String#>, column5: <#T##String#>, column6: <#T##String#>),
+    Pronomen(column1: <#T##String#>, column2: <#T##String#>, column3: <#T##String#>, column4: <#T##String#>, column5: <#T##String#>, column6: <#T##String#>),
+    
+    ]
+   */
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            
+        }
     }
 }
 
